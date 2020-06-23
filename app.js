e86e2cfb9f017f79055acb8205595b25520b4c78
Trevor Park <trevorme91@gmail.com>
2020-06-23 14:14:06 +0900

    change path

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ function init() {
 
   //Load Model
   let loader = new THREE.GLTFLoader();
-  loader.load("./house/cube.gltf", function (gltf) {
+  loader.load("./house/scene.gltf", function (gltf) {
     scene.add(gltf.scene);
     house = gltf.scene.children[0];
     animate();
